51caf15d6a7b28e8a0f7c883c48f3e680c22fe86
Jules <jgmz.jg@gmail.com>
2017-03-28 15:29:11 -0500

    Add comments

--- gallery.js
+++ gallery.js
@@ -1,4 +1,4 @@
-
+// These are my cat photos
 var catCollection = ['http://bit.ly/2o3ftDx',
 					 'http://bit.ly/2nfZKxg',
 					 'http://bit.ly/2nJfKLj',
@@ -6,9 +6,10 @@ var catCollection = ['http://bit.ly/2o3ftDx',
 					 'http://bit.ly/2nJcy2r',
 					 'http://bit.ly/2nrTyTC']
 
+//This should make the photo change with every click
 function goForward(){
 	for (var i = 0; i < catCollection.length; i++){
-	var kitty = document.getElementById("display").innerHTML = '<img src=' + catCollection[i] + '>';
+	var kitty = '<img src=' + catCollection[i] + '>'
     }
-    return kitty;
+    return document.getElementById("display").innerHTML = kitty;
 }
