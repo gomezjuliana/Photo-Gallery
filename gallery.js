5e64d68cfc84bfea0cc82f11cae367846ac55f0b
Jules <jgmz.jg@gmail.com>
2017-03-29 12:51:14 -0500

    Playing with the goBackward function

--- gallery.js
+++ gallery.js
@@ -6,11 +6,24 @@ var catCollection = ['http://bit.ly/2o3ftDx',
 					 'http://bit.ly/2nJcy2r',
 					 'http://bit.ly/2nrTyTC']
 
-//This should make the photo change with every click
+//This makes the photo change with every click
 	var count = 0;
 function goForward(){
     document.getElementById("display").src = catCollection[count];
 	count = (count >= catCollection.length-1) ? 0 : count + 1;
+	console.log(count);
+}
+
+function goBackward(){
+	if (count = -1){
+	   count = 5;
+	   document.getElementById("display").src = catCollection[count];
+	   console.log(count, catCollection[count]);
+	} else if (count <= catCollection.length){
+		count = count -1;
+		document.getElementById("display").src = catCollection[count];
+		console.log(count, catCollection[count]);
+	}	
 }
 
 document.addEventListener('DOMContentLoaded', goForward);
