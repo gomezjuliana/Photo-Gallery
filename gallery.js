b722a33ea3ee2d9cf75ae515c428ae818210b287
Jules <jgmz.jg@gmail.com>
2017-03-31 10:40:57 -0500

    Work on animation to go backwards

--- gallery.js
+++ gallery.js
@@ -9,20 +9,30 @@ var catCollection = ['http://bit.ly/2o3ftDx',
 // These functions make the gallery move
 var count = -1;
 function goForward(){
+	display.classList.add('woem');
 	count = count >= catCollection.length-1 ? 0 : count +1;
 	document.getElementById('display').src = catCollection[count];
+	document.getElementById('display').addEventListener('animationend', removeTransition2);
 
 }
 
+function removeTransition(e){
+	if (e.type !== 'animationend') return;
+	this.classList.remove('meow');
+}
 
+function removeTransition2(e){
+	if (e.type !== 'animationend') return;
+	this.classList.remove('woem');
+}
 
 function goBackward(){
 	display.classList.add('meow');
 	count = count <= 0 ? catCollection.length-1 : count -1;
 	document.getElementById('display').src = catCollection[count];
+	document.getElementById('display').addEventListener('animationend', removeTransition);
+    ;
 }
-
-display.classList.remove('meow');
 
 // This loads the first photo once the page is loaded
 document.addEventListener('DOMContentLoaded', goForward);
