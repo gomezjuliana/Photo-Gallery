01f4459d1b350d2832c365b73baa02d54e4707df
Jules <jgmz.jg@gmail.com>
2017-04-03 22:39:25 -0500

    Finish animations

--- gallery.js
+++ gallery.js
@@ -7,32 +7,39 @@ var catCollection = ['http://bit.ly/2o3ftDx',
 					 'http://bit.ly/2nrTyTC']
 
 // These functions make the gallery move
-var count = -1;
+var count = 0;
+
+var prevImg = document.querySelector('.gallery-prev');
+var currImg = document.querySelector('.gallery-curr');
+var nextImg = document.querySelector('.gallery-next');
+
 function goForward(){
-	display.classList.add('woem');
+	nextImg.classList.add('gallery-next-curr');
+	currImg.classList.add('gallery-curr-prev');
 	count = count >= catCollection.length-1 ? 0 : count +1;
-	document.getElementById('display').src = catCollection[count];
-	document.getElementById('display').addEventListener('animationend', removeTransition2);
 
-}
-
-function removeTransition(e){
-	if (e.type !== 'animationend') return;
-	this.classList.remove('meow');
-}
-
-function removeTransition2(e){
-	if (e.type !== 'animationend') return;
-	this.classList.remove('woem');
+	currImg.addEventListener('transitionend', setup);
 }
 
 function goBackward(){
-	display.classList.add('meow');
+	prevImg.classList.add('gallery-prev-curr');
+	currImg.classList.add('gallery-curr-next');
 	count = count <= 0 ? catCollection.length-1 : count -1;
-	document.getElementById('display').src = catCollection[count];
-	document.getElementById('display').addEventListener('animationend', removeTransition);
-    ;
+
+	currImg.addEventListener('transitionend', setup);
+}
+
+function setup() {
+	const prev = count-1 < 0 ? catCollection.length-1 : count-1;
+	const next = count+1 >= catCollection.length ? 0 : count+1;
+	prevImg.src = catCollection[prev];
+	currImg.src = catCollection[count];
+	nextImg.src = catCollection[next];
+	prevImg.classList.remove('gallery-prev-curr');
+	nextImg.classList.remove('gallery-next-curr');
+	currImg.classList.remove('gallery-curr-prev', 'gallery-curr-next');
+
 }
 
 // This loads the first photo once the page is loaded
-document.addEventListener('DOMContentLoaded', goForward);
+document.addEventListener('DOMContentLoaded', setup);
